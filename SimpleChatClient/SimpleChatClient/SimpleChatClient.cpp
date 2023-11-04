37a03f80bd4bd3fc3ae71984dec7bc885507c37e
unknown <starpro123@naver.com>
2023-11-04 18:13:09 +0900

    Client - Sever send, recv 구현

--- SimpleChatClient/SimpleChatClient/SimpleChatClient.cpp
+++ SimpleChatClient/SimpleChatClient/SimpleChatClient.cpp
@@ -14,7 +14,7 @@ int main(int argc, char ** argv)
     startUpResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
     if (startUpResult == 0) {
-        std::cout << "result 초기화 완료" << std::endl;
+        std::cout << "클라이언트 초기화 완료" << std::endl;
     }
     else {
         std::cout << "rfsflt 초기화 안됨 reason: " << startUpResult << std::endl;
@@ -64,7 +64,6 @@ int main(int argc, char ** argv)
     }
 
     constexpr int recvbuflen = 512;
-
     const char* sendbuf = "this is a test";
     char recvbuf[recvbuflen];
 
@@ -77,20 +76,16 @@ int main(int argc, char ** argv)
         return 1;
     }
 
+    iResult = shutdown(connectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed: %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
 
     freeaddrinfo(result);
 
     WSACleanup();
     return 0;
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
