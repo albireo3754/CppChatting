ef5ed7e64e1e6d2637239782f868303f7937378a
alibreo3754 <starpro123@naver.com>
2023-12-25 14:10:53 +0900

    init project

---
+++ KotlinChat/src/main/kotlin/com/albireo3754/Chat/ChatApplication.kt
@@ -0,0 +1,11 @@
+package com.albireo3754.Chat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChatApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChatApplication>(*args)
+}
---
+++ KotlinChat/src/test/kotlin/com/albireo3754/Chat/ChatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.albireo3754.Chat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
